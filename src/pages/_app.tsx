8c610f50616757e9dfab201da88b632f0167c1cf
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-12-04 15:33:40 +0700

    Adding icon, og and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import 'styles/tailwind.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <div className="antialiased">
+    <Head>
+      <title>PEPPAPIGHS</title>
+    </Head>
     <Component {...pageProps} />
   </div>
 )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,21 @@ export default class Document extends NextDocument {
             type="font/woff2"
             crossOrigin="true"
           />
+          <link rel="shortcut icon" href="/favicon.png" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=yes"
+          />
+          <meta name="theme-color" content="black" />
+          <meta property="og:title" content="PEPPAPIGHS" />
+          <meta
+            property="og:image"
+            content="https://pontakorn.vercel.app/og.png"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://pontakorn.vercel.app" />
+          <meta name="description" content="PEPPAPIGHS" />
         </Head>
         <body>
           <Main />
